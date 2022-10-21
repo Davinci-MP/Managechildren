29a8bca92012f456bc652e12e243f2918510ad78
vodaithang <vodaithang1609@gmail.com>
2022-10-21 23:04:07 +0700

    doan a

--- app/src/main/java/com/example/doancuoiky/activity/ManageStudentsActivity.java
+++ app/src/main/java/com/example/doancuoiky/activity/ManageStudentsActivity.java
@@ -1,16 +1,48 @@
 package com.example.doancuoiky.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.doancuoiky.R;
+import com.example.doancuoiky.adapter.ClassesAdapter;
+import com.example.doancuoiky.model.Classes;
+import com.example.doancuoiky.sqlite.ClassesDAO;
 
-public class ManageStudentsActivity extends AppCompatActivity {
+import java.util.List;
+
+public class ManageStudentsActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText etStudentId, etName, etDob;
+    private Spinner spClasses;
+    private List<Classes> classesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_students);
+        etStudentId = findViewById(R.id.etStudentsID);
+        etName = findViewById(R.id.etName);
+        etDob = findViewById(R.id.etDob);
+        spClasses = findViewById(R.id.spClasses);
+
+        fillClassesToSpiner();
+
+        findViewById(R.id.btSave).setOnClickListener(this);
+    }
+    private void fillClassesToSpiner(){
+        ClassesDAO dao = new ClassesDAO(this);
+        classesList = dao.getAll();
+        ClassesAdapter classesAdapter = new ClassesAdapter(this,classesList);
+        spClasses.setAdapter(classesAdapter);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btSave:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/doancuoiky/sqlite/StudentDAO.java
+++ app/src/main/java/com/example/doancuoiky/sqlite/StudentDAO.java
@@ -1,4 +1,30 @@
 package com.example.doancuoiky.sqlite;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.doancuoiky.helper.DateTimeHelper;
+import com.example.doancuoiky.model.Classes;
+import com.example.doancuoiky.model.Student;
+
 public class StudentDAO {
+
+    private SQLiteDatabase db;
+
+    public StudentDAO(Context context) {
+        DbHelper helper = new DbHelper(context);
+
+        this.db = helper.getWritableDatabase();
+
+    }
+    public long insert(Student emp){
+        ContentValues values = new ContentValues();
+        values.put("id",emp.getId());
+        values.put("name",emp.getName());
+        values.put("dob", DateTimeHelper.toString(emp.getDob()));
+        values.put("classid",emp.getClassID());
+
+        return db.insert("students",null,values);
+    }
 }
