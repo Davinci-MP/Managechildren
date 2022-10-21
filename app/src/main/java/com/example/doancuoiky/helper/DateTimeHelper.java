47f77119a1641dbb3ffbe6794cf705e93db770ee
vodaithang <vodaithang1609@gmail.com>
2022-10-21 23:04:07 +0700

    doan a

--- app/src/main/java/com/example/doancuoiky/helper/DateTimeHelper.java
+++ app/src/main/java/com/example/doancuoiky/helper/DateTimeHelper.java
@@ -1,4 +1,12 @@
 package com.example.doancuoiky.helper;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTimeHelper {
+    private final  String Pattern = "dd/MM/yyyy";
+    public Date toDate(String st) {
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        return null;
+    }
 }
